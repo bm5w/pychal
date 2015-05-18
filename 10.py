eba9b1cc7077b6a24e4849d146770fe703d8f8d6
Mark Saiget <saiget@gmail.com>
2015-05-18 15:20:06 -0700

    Add 10.py, plus prime method.

--- 10.py
+++ 10.py
@@ -2,9 +2,27 @@
 http://www.pythonchallenge.com/pc/return/bull.html"""
 
 
-def main():
-    pass
+def main(n=50):
+    a = [1, 11, 21, 1211, 111221]
+    while len(a) <= 50:
+        index = len(a)-1
+        temp_a = len(str(a[index-1]))
+        temp_b = len(str(a[index]))
+        y = temp_a + temp_b
+        x = 10**(y-1)
+        a.append(x)
+    for num, i in enumerate(a):
+        print 'num: {}, len: {}'.format(num, len(str(i)))
+    return a[50]
 
+
+def prime(x):
+    """Return true if prime."""
+    for i in xrange(2, x//2):
+        if x % i == 0:
+            print 'x: {}, i: {}'.format(x, i)
+            return False
+    return True
 
 if __name__ == "__main__":
     main()
