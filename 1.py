05e219129236a869924931755ef04fb4f1254511
Mark Saiget <saiget@gmail.com>
2015-05-17 17:33:19 -0700

    Translate url.

--- 1.py
+++ 1.py
@@ -20,3 +20,4 @@ def uncode(str_in):
 
 if __name__ == "__main__":
     print uncode("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.")
+    print uncode("map")
