3925b81b38d9e4f4f10bfdd6eb99369309108cdf
Mark Saiget <saiget@gmail.com>
2015-05-17 19:39:59 -0700

    working 5.py

--- 5.py
+++ 5.py
@@ -6,8 +6,9 @@ import pickle
 
 def main():
     out = urllib.urlopen('http://www.pythonchallenge.com/pc/def/banner.p')
-    out = out.read()
-    print pickle.loads(out)
+    out_dict = pickle.load(out)
+    for x in out_dict:
+        print ''.join(y[0]*y[1] for y in x)
 
 
 if __name__ == "__main__":
