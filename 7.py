ff8d1aa076f9accdc834c61e40d3237b73792891
Mark Saiget <saiget@gmail.com>
2015-05-17 21:14:55 -0700

    solved 7

--- 7.py
+++ 7.py
@@ -1,10 +1,21 @@
 """Python challenge #7:
 http://www.pythonchallenge.com/pc/def/oxygen.html"""
+import urllib
+from PIL import Image
 
 
 def main():
-    pass
+    png = urllib.urlopen('http://www.pythonchallenge.com/pc/def/oxygen.png')
+    png = Image.open(png)
+    output = []
+    png_matrix = png.load()
+    for x in xrange(0, int(png.size[0]), 7):
+        temp = png_matrix[x, 47]
+        output.append(chr(temp[0]))
+    return ''.join(output)
 
 
 if __name__ == "__main__":
-    main()
+    output = main()
+    array = [105, 110, 116, 101, 103, 114, 105, 116, 121]
+    print ''.join([chr(x) for x in array])
