d20128972efc87548af019968e506af41b19a706
Mark Saiget <saiget@gmail.com>
2015-05-18 16:49:03 -0700

    solve 11.

--- 11.py
+++ 11.py
@@ -1,7 +1,7 @@
 """Python challenge #11:
 http://www.pythonchallenge.com/pc/return/cave.jpg"""
 import urllib2
-from PIL import Image
+from PIL import Image, ImageDraw
 url = 'http://www.pythonchallenge.com/pc/return/cave.jpg'
 un = 'huge'
 pw = 'file'
@@ -12,13 +12,22 @@ def main():
     img = urllib2.urlopen(url)
     im = Image.open(img)
     png_matrix = im.load()
+    draw = ImageDraw.Draw(im)
+    odd_even = 0
     for row in xrange(int(im.size[1])):
         for column in xrange(int(im.size[0])):
-            print png_matrix[row, column]
-        import pdb; pdb.set_trace()
+            if column % 2 == odd_even:
+                draw.point([(column/2, row/2)], png_matrix[column, row])
+        # alternate from odd to even or even to odd
+        if odd_even == 0:
+            odd_even = 1
+        else:
+            odd_even = 0
+    im.show()
 
 
 def setup_auth_handler():
+    """Method for setting up authentication."""
     password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
     password_mgr.add_password(None, url, un, pw)
     handler = urllib2.HTTPBasicAuthHandler(password_mgr)
