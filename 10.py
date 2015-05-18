22eb994d38cbd071ffc1c3fd4d4506be0b0e8cb8
Mark Saiget <saiget@gmail.com>
2015-05-18 15:44:15 -0700

    solved 10.

--- 10.py
+++ 10.py
@@ -2,34 +2,29 @@
 http://www.pythonchallenge.com/pc/return/bull.html"""
 
 
-def main(n=50):
+def main(n=30):
     a = [1, 11, 21, 1211, 111221]
-    while len(a) <= 50:
-        index = len(a)-1
-        temp_a = len(str(a[index-1]))
-        temp_b = len(str(a[index]))
-        y = temp_a + temp_b
-        x = 10**(y-1)
-        a.append(x)
-    for num, i in enumerate(a):
-        print 'num: {}, len: {}'.format(num, len(str(i)))
-    return a[50]
+    while len(a) <= n:
+        a.append(look_and_say_next(a[len(a)-1]))
+    print 'len(a[30]): {}'.format(len(a[30]))
 
 
 def look_and_say_next(inp):
     """Given current number, get next in Look-and-say sequence."""
     output = ''
-    for index, curr in enumerate(str(inp)):
-        count = 0
-        if index == 0:
-            previous = curr
+    count = 1
+    previous = str(inp)[0]
+    for index, curr in enumerate(str(inp)[1:], start=1):
+        if previous == curr:
+            count += 1
         else:
-            if previous == curr:
-                count += 1
-            else:
-                output += count
-                output += previous
-                previous = curr
+            output += str(count)
+            output += str(previous)
+            previous = curr
+            count = 1
+    # add last number on
+    output += str(count)
+    output += str(curr)
     return output
 
 if __name__ == "__main__":
