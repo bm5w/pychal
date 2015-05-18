9d5980a276ac9de52898690da52ad8946e00cc0f
Mark Saiget <saiget@gmail.com>
2015-05-17 17:31:35 -0700

    More efficient solution for 1.py, using string.maketrans and string.translate.

--- 1.py
+++ 1.py
@@ -1,17 +1,21 @@
 """Python challenge #1: http://www.pythonchallenge.com/pc/def/map.html"""
+import string
 
 
 def uncode(str_in):
-    output = ''
-    for x in str_in:
-        if x.isalnum():
-            temp = (chr(ord(x)+2))
-            if not temp.isalnum():
-                temp = chr(ord(temp)-26)
-            output += temp
-        else:
-            output += x
-    return output
+    # output = ''
+    # for x in str_in:
+    #     if x.isalnum():
+    #         temp = (chr(ord(x)+2))
+    #         if not temp.isalnum():
+    #             temp = chr(ord(temp)-26)
+    #         output += temp
+    #     else:
+    #         output += x
+    # return output
+    lowercase = string.ascii_lowercase
+    table = string.maketrans(lowercase, lowercase[2:]+lowercase[:2])
+    return string.translate(str_in, table)
 
 
 if __name__ == "__main__":
