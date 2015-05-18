762efabed9e48d3b9e2a51676ed7cf85efeb5d55
Mark Saiget <saiget@gmail.com>
2015-05-17 18:50:59 -0700

    workingish 4.py

--- 4.py
+++ 4.py
@@ -6,4 +6,16 @@ import urllib
 def main(num=12345):
     out = urllib.urlopen(("http://www.pythonchallenge.com/pc/def/"
                           "linkedlist.php?nothing={}").format(num))
-    import pdb; pdb.set_trace()
+    temp = out.readline()
+    print temp
+    num = ''.join([x for x in temp if x.isdigit()])
+    try:
+        main(int(num))
+    except ValueError:
+        print out.read()
+        temp = out.url
+        num = ''.join([x for x in temp if x.isdigit()])
+        main(int(num)/2)
+
+if __name__ == "__main__":
+    main()
