9a8db5e8f261fb288c34766cf42e2b14fa38fb0b
Mark Saiget <saiget@gmail.com>
2015-05-18 22:32:42 -0700

    solve 15

--- 15.py
+++ 15.py
@@ -1,11 +1,14 @@
 """Python challenge #15:
 http://www.pythonchallenge.com/pc/return/uzi.html
 """
+from datetime import date
 
 
 def main():
-    pass
-
+    for x in range(100):
+        year = 1006 + 10*x
+        if date(year, 1, 26).weekday() == 0:
+            print year
 
 if __name__ == '__main__':
     main()
